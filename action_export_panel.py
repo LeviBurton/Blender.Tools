d16f6198e9da71bb7d0710c7140ef73d7e12fe9e
LeviBurton <levi.burton@gmail.com>
2018-04-26 22:56:05 -0500

    Added save path option and game engine selection to action export.

--- action_export_panel.py
+++ action_export_panel.py
@@ -1,3 +1,9 @@
+bl_info = {
+    "name": "Action-Export",
+    "category": "Object",
+}
+
+
 import bpy
 import sys
 import os
@@ -43,13 +49,19 @@ class ActionExportPanel_export(bpy.types.Operator):
         for action in bpy.data.actions:
             if action.EX_export_this == True:
                 context.object.animation_data.action = action
-                path = bpy.path.abspath("//Export\\FBX\\Action\\" + action.name + ".fbx")
-                bpy.ops.export_scene.fbx(filepath=path,
-                    use_default_take=False,
-                    use_selection=True,
-                    use_anim_action_all=False,
-                    use_mesh_edges=True,
-                    object_types={'MESH','ARMATURE'})
+                path = bpy.path.abspath(bpy.context.scene.conf_path + action.name + ".fbx")
+                
+                if context.scene.engine_export_enum == 'Unity':
+                    bpy.ops.export_scene.fbx(filepath=path,
+                        use_default_take=False,
+                        use_selection=True,
+                        use_armature_deform_only=True,
+                        use_anim_optimize=True, 
+                        use_anim_action_all=False,
+                        use_mesh_edges=True,
+                        axis_up="Y",
+                        axis_forward="-Z",
+                        object_types={'MESH','ARMATURE'})
         
         context.object.animation_data.action = start_action
         
@@ -67,16 +79,22 @@ class ActionExportPanel(bpy.types.Panel):
         layout = self.layout
         wm = context.window_manager
         row = layout.row(align=True)
+        row = layout.row(align=True)
         row.operator("action_export_panel.export")
         row = layout.row()
-        
+      
         #row.label(text=' Selected Actions:')
-       
+
         row = layout.row()
         col = row.column()
         col.operator("action_export_panel.select_all")
         col = row.column()
         col.operator("action_export_panel.select_none")
+       
+        col = layout.column()
+        col.prop(context.scene, 'conf_path')
+            
+        layout.prop(context.scene, "engine_export_enum", text="Engine: ")
         
         for action in bpy.data.actions:
            row = layout.row(align=True)
@@ -88,10 +106,26 @@ def register():
     bpy.utils.register_class(ActionExportPanel_select_none)
     bpy.utils.register_class(ActionExportPanel_export)
     
+    engines = [
+        ("UE4","UE4", "UE4"),
+        ("Unity", "Unity", "Unity"),  
+    ]
+
+    bpy.types.Scene.engine_export_enum = bpy.props.EnumProperty(items=engines)
+    bpy.types.Scene.action_export_path = bpy.props.StringProperty \
+      (
+      name = "Export Path",
+      default = bpy.path.abspath("//Export\\FBX\\Actions\\"),
+      description = "Define the export path of your fbx files.",
+      subtype = 'DIR_PATH'
+      )
     bpy.types.Action.EX_export_this = bpy.props.BoolProperty(default=False)
-    
+    bpy.utils.register_module(__name__)
+
 def unregister():
    return
 
 #if __name__ == "__main__":
 register()
+
+   
